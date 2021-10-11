c7b534a149ea133644c16eb286da523b26feb781
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-10-11 14:54:14 -0400

    imported template and data

---
+++ hw2.py
@@ -0,0 +1,20 @@
+def test_knn(train_x, train_y, test_x, num_nn):
+	return None
+
+def train_pocket(train_x, train_y, num_iters):
+	return None
+
+def test_pocket(w, test_x):
+	return None
+
+def compute_accuracy(test_y, pred_y):
+	return None
+
+def get_id():
+	return None
+
+def main():
+	return None
+
+if __name__ == "__main__":
+    main()
